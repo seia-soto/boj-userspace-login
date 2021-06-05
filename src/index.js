e0f3cd0f2b46fd93c1e5000c13fa6032ac42dd9a
Seia-Soto <seia@outlook.kr>
2021-06-06 02:54:07 +0900

    feat: move to puppeteer-extra for recaptcha

--- src/index.js
+++ src/index.js
@@ -1,39 +1,36 @@
-const { chromium: app } = require('playwright')
+const app = require('puppeteer-extra')
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 
 const prepare = async page => {
   try {
+    await page.setViewport({
+      width: 500,
+      height: 750,
+      deviceScaleFactor: 1
+    })
     await page.bringToFront()
 
     await page.goto('https://www.acmicpc.net/login?next=' + encodeURIComponent('/'))
-
-    await page.click('text=로그인 상태 유지')
-    await page.click('[placeholder="아이디 / 이메일"]')
   } catch (error) {
     console.error(error)
   }
 }
 
 const init = async callback => {
+  app.use(StealthPlugin())
+
   const browser = await app.launch({
     headless: false,
+    args: ['--window-size=500,750'],
     timeout: 10 * 1000
   })
-  const context = await browser.newContext({
-    viewport: {
-      width: 500,
-      height: 750
-    }
-  })
-  const page = await context.newPage()
+  const page = await browser.newPage()
 
   let result = null
 
   browser.on('disconnected', () => callback(result))
 
-  context.on('close', async () => await browser.close())
-  context.on('page', page => page.close())
-
-  page.on('close', async () => await context.close())
+  page.on('close', () => browser.close())
   page.on('response', async response => {
     const url = response.url()
 
